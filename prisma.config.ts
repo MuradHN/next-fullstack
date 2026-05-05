7ba1549f55e4af3020a575c2f9bca2565af28f29
Murad <muradhn2k2@gmail.com>
2026-05-05 11:10:03 +0700

    feat: Build & Deploy

--- prisma.config.ts
+++ prisma.config.ts
@@ -1,7 +1,4 @@
-import process from "node:process";
 import { defineConfig, env } from "prisma/config";
-
-process.loadEnvFile?.();
 
 export default defineConfig({
   schema: "prisma/schema.prisma",
